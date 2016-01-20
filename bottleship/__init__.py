abdf8a26c828cab49a0f2aa56a0b0c83bc986969
omtinez <omtinez@gmail.com>
2016-01-19 22:53:16 -0800

    Bump to version 2.0.1

--- bottleship/__init__.py
+++ bottleship/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Oscar Martinez'
 __email__ = 'omtinez@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __all__ = ['BottleShip', 'data_encode', 'data_decode', 'data_is_encoded']
 
 from .bottleship import BottleShip, data_encode, data_decode, data_is_encoded
