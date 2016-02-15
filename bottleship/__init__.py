e7d987bbc376d448a786e395dc8ef54e416a8b55
omtinez <omtinez@gmail.com>
2016-02-14 22:28:20 -0800

    Bump version: 0.2.1 → 0.2.2

--- bottleship/__init__.py
+++ bottleship/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Oscar Martinez'
 __email__ = 'omtinez@gmail.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __all__ = ['BottleShip', 'data_encode', 'data_decode', 'data_is_encoded', 'parse_auth', 'static_file', 'redirect']
 
 from .bottleship import BottleShip, data_encode, data_decode, data_is_encoded, parse_auth, static_file, redirect
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='bottleship',
-    version='0.2.1',
+    version='0.2.2',
     description="Authentication for the Bottle web framework meant for very simple workflows and little-to-none user interaction",
     long_description=readme + '\n\n' + history,
     author="Oscar Martinez",
